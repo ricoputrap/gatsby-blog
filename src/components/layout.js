e47a3d2c1e35df3f601e1748537b595a0182ac71
Rico Putra Pradana <rico.putra@outlook.co.id>
2021-06-19 17:18:24 +0700

    Implement query without useStaticQuery on blog page

--- src/components/layout.js
+++ src/components/layout.js
@@ -26,10 +26,19 @@ const Layout = ({ pageTitle, children }) => {
       <nav>
         <ul className={navLinks}>
           <li className={navLinkItem}>
-            <Link to='/' className={navLinkText}>Home</Link>
+            <Link to='/' className={navLinkText}>
+              Home
+            </Link>
           </li>
           <li className={navLinkItem}>
-            <Link to='/about' className={navLinkText}>About</Link>
+            <Link to='/about' className={navLinkText}>
+              About
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to='/blog' className={navLinkText}>
+              Blog
+            </Link>
           </li>
         </ul>
       </nav>
---
+++ src/pages/blog.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import Layout from '../components/layout';
+
+const BlogPage = ({ data }) => {
+  return (
+    <Layout pageTitle='My Blog Posts'>
+      <ul>
+        {data.allFile.nodes.map(post => (
+          <li key={post.name}>
+            { post.name }
+          </li>
+        ))}
+      </ul>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`;
+
+export default BlogPage;
