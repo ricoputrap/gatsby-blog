7f5f44f4c432e06e78c3a152b4c4c8f524773c95
Rico Putra Pradana <rico.putra@outlook.co.id>
2021-06-19 13:52:46 +0700

    Implement link as navigation to other page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 const IndexPage = () => {
   return (
     <main>
       <title>Home Page</title>
       <h1>Welcome to my Gatsby Blog!</h1>
+      <Link to='/about'>About</Link>
       <p>I'm making this by following the Gatsby Tutorial.</p>
     </main>
   )
